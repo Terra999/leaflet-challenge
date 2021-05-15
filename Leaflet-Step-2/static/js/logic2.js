39b949a297333db778a6043a42177149fc36fe5e
Terra Vaughn <vterra28@yahoo.com>
2021-05-15 17:52:48 -0500

    got all 3 base maps to load

--- Leaflet-Step-2/static/js/logic2.js
+++ Leaflet-Step-2/static/js/logic2.js
@@ -1,4 +1,4 @@
-console.log("terra.js loaded successfully!");
+console.log("logic2.js loaded successfully!");
 
 // Function to set color based on earthquake depth
 function getColor(depth) {
@@ -46,7 +46,7 @@ var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/satellite",
+  id: "mapbox/satellite-streets-v9",
   accessToken: API_KEY
 });
 
@@ -64,7 +64,7 @@ var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/outdoors",
+  id: "mapbox/outdoors-v10",
   accessToken: API_KEY
 });
 
@@ -89,64 +89,60 @@ var myMap = L.map("map", {
     37.09, -95.71
   ],
   zoom: 5,
-  layers: [
-    layers
-  ]
+  layers: [satellite]
 });
 
 // Pass our map layers into our layer control
 // Add the layer control to the map
-L.control.layers(baseMaps, null, {
-  collapsed: false
-}).addTo(myMap);
+L.control.layers(baseMaps, null).addTo(myMap);
 
-// Store API endpoint inside baseURL
-var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// // Store API endpoint inside baseURL
+// var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-var tectonicPlates = "static/data/PB2002_boundaries.json";
+// var tectonicPlates = "static/data/PB2002_boundaries.json";
 
-// Grab the data with d3
-d3.json(baseURL).then(function(data) {
+// // Grab the data with d3
+// d3.json(baseURL).then(function(data) {
   
-  d3.json(tectonicPlates).then(function(te))
+//   d3.json(tectonicPlates).then(function(te))
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  L.geoJSON(data, {
-    pointToLayer: function (feature, latlng) {
-      // Add circlse to map
-      return L.circleMarker(latlng);
-    },
-    // Call style from function
-    style: style,
-    // Bind a popup to the circles to show magnitude, depth, and place
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +
-      "</h3><h3>Depth: " + feature.geometry.coordinates[2] + "</h3><hr><h3>Place: " +
-      feature.properties.place + "</h3>")
-    }
-  }).addTo(myMap); 
+//   // Create a GeoJSON layer containing the features array on the earthquakeData object
+//   L.geoJSON(data, {
+//     pointToLayer: function (feature, latlng) {
+//       // Add circlse to map
+//       return L.circleMarker(latlng);
+//     },
+//     // Call style from function
+//     style: style,
+//     // Bind a popup to the circles to show magnitude, depth, and place
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +
+//       "</h3><h3>Depth: " + feature.geometry.coordinates[2] + "</h3><hr><h3>Place: " +
+//       feature.properties.place + "</h3>")
+//     }
+//   }).addTo(myMap); 
 
-});
+// });
 
-// Create a legend to display depth scale/color
-var legend = L.control({ position: 'bottomright' });
-// When the layer control is added, insert a div with the class of "info legend"
-legend.onAdd = function () {
+// // Create a legend to display depth scale/color
+// var legend = L.control({ position: 'bottomright' });
+// // When the layer control is added, insert a div with the class of "info legend"
+// legend.onAdd = function () {
 
-  var div = L.DomUtil.create('div', 'info legend'),
-    grades = [-10, 10, 30, 50, 70, 90],
-    labels = [];
+//   var div = L.DomUtil.create('div', 'info legend'),
+//     grades = [-10, 10, 30, 50, 70, 90],
+//     labels = [];
 
-  // loop through our depth intervals and generate a label with a colored square for each interval
-  for (var i = 0; i < grades.length; i++) {
-    div.innerHTML +=
-      '<i style="background:' + getColor(grades[i] +1) + '"></i> ' +
-      grades[i] + (grades[i +1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-  }
-  return div;
-};
+//   // loop through our depth intervals and generate a label with a colored square for each interval
+//   for (var i = 0; i < grades.length; i++) {
+//     div.innerHTML +=
+//       '<i style="background:' + getColor(grades[i] +1) + '"></i> ' +
+//       grades[i] + (grades[i +1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+//   }
+//   return div;
+// };
 
-// Add legend to my map
-legend.addTo(myMap);
+// // Add legend to my map
+// legend.addTo(myMap);
 
 
